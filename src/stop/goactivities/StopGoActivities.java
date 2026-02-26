67538a9c6a16d0363ada6ad53b6bad458848430b
ST10506055 <st10506055@myemeris.edu.za>
2026-02-26 08:05:25 +0200

    FIRST PUSH
    ADDING COMMENTS

---
+++ src/stop/goactivities/StopGoActivities.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package stop.goactivities;
+
+/**
+ *
+ * @author ST10506055
+ * ALEX RADOVANOVIC
+ * ACTIVITY 1, STOP AND GO SIGNS 
+ * 
+ */
+public class StopGoActivities {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
